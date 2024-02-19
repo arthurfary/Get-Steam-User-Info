04a3764446f530f5a9dfd94fd4d71b97ed7750dd
arhturfary <arthurfary22@gmail.com>
2024-02-18 23:51:55 -0300

    removed caching from inside class (now fully handeled by flask cahing) + refactor

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Get-Steam-User-Info
-A Easy to use, non-cluttered, Flask Api that retrieves a Steam User's data.
+### API Purpose:
+
+-   Retrieves information about Steam games and user game libraries using the Steam Web API in a non cluttered way.
+
+### Endpoints:
+
+| Endpoint | Method | Description |
+|---|---|---|
+| `/games/<steam_id_or_vanity_url>` | GET | Retrieves a list of games owned by a given Steam user. |
+| `/games/recent/<steam_id_or_vanity_url>` | GET | Retrieves a list of recently played games for a Steam user. |
+| `/games/gameinfo/<steam_id_or_vanity_url>/gameid` | GET | Retrieves specific information about a game owned by a Steam user. |
+| `/games/getAppidNamePair` | GET | Returns a dictionary mapping Steam app IDs to game names. |
--- modules/games/SteamGameRequests.py
+++ modules/games/SteamGameRequests.py
@@ -1,48 +1,54 @@
 from ..SteamRequest import SteamRequest
-
 from flask import jsonify
 from markupsafe import escape
 
 class SteamGameRequests(SteamRequest):
-    steam_games = None
-    owned_games = None
+    '''
+    This class contains the functions for requesting game data from steam.
+    '''
 
-    def get_steam_games(self):
-        if self.steam_games == None:
-            data = SteamRequest.make_steam_request("ISteamApps/GetAppList/v2", {})
-            all_games = data['applist']['apps']
-            appid_game_pair = {game['appid']: game['name'] for game in all_games}
-            self.steam_games = appid_game_pair
-        return self.steam_games
 
-    def get_owned_games(self, steamId):
-        if self.owned_games == None:
-            data = SteamRequest.make_steam_request("IPlayerService/GetOwnedGames/v0001", {"steamId": escape(steamId)})['response']['games']
-            appid_keyed_dict = {
-                game['appid']: {
-                    # "name": game['name'],
-                    "playtime_disconnected": game['playtime_disconnected'],
-                    "playtime_forever": game['playtime_forever'],
-                    "playtime_linux_forever": game['playtime_linux_forever'],
-                    "playtime_mac_forever": game['playtime_mac_forever'],
-                    "playtime_windows_forever": game['playtime_windows_forever'],
-                    "rtime_last_played": game['rtime_last_played']
-                } for game in data
+    def _get_steam_games(self):
+        data = SteamRequest.make_steam_request("ISteamApps/GetAppList/v2", {})
+        all_games = data['applist']['apps']
+        steam_games = {game['appid']: game['name'] for game in all_games}
+        return steam_games
+
+    def _get_owned_games(self, steamId):
+        data = SteamRequest.make_steam_request("IPlayerService/GetOwnedGames/v0001", {"steamId": escape(steamId)})['response']['games']
+        owned_games = {
+            game['appid']: {
+                "playtime_forever": game['playtime_forever'],
+                **{ # ** decompresses the rest if the return if there is one
+                    key: game[key] for key in ("playtime_disconnected", "playtime_linux_forever", "playtime_mac_forever", "playtime_windows_forever", "rtime_last_played")
+                    if key in game
+                }
             }
-            self.owned_games = appid_keyed_dict
-        return self.owned_games
+            for game in data
+        }
+        return owned_games
+
+    def _append_names(self, owned_games, steam_games):
+        for appid, game_data in owned_games.items():
+            game_name = steam_games.get(appid)
+            if game_name:
+                game_data['name'] = game_name
+        return owned_games
+    
+    def _get_games(self, steamId):
+        owned_games = self._get_owned_games(steamId)
+        steam_games = self._get_steam_games()
+        self._append_names(owned_games, steam_games) 
+        return owned_games
+
+    ###
 
     def get_appid_name_pair(self):
-        return self.get_steam_games()
+        steam_games = self._get_steam_games()
+        return steam_games
 
     def get_user_owned_games(self, steamId):
-        owned_games = self.get_owned_games(steamId)
-        steam_games = self.get_steam_games()
-
-        for owned_game in owned_games:
-            if owned_game in steam_games:
-                owned_games[owned_game]['name'] = steam_games[owned_game]
-                
+        owned_games = self._get_games(steamId)
         return jsonify(owned_games)
 
     def get_recently_played_games(self, steamId):
@@ -50,11 +56,11 @@ class SteamGameRequests(SteamRequest):
         return jsonify(recent_games)
 
     def get_specific_game_info(self, steamId, gameId: int):
-        owned_games = self.get_owned_games(steamId)
-        steam_games = self.get_steam_games()
+        owned_games = self._get_games(steamId)
+        specific_game = owned_games.get(int(gameId))  # need to make a more explicit error handler
 
-        for owned_game in owned_games:
-            if owned_game in steam_games:
-                owned_games[owned_game]['name'] = steam_games[owned_game]
-    
-        return jsonify(owned_games[int(gameId)])
+        if specific_game:
+            return jsonify(specific_game)
+        else:
+            # Handle game not found error
+            return "Key not found"
